31c282ded12022a07d9a7c5b404e26c5f94a5a4a
VitorFormiga <jvformiga@gmail.com>
2025-06-11 10:54:43 -0300

    11.06.2025  16

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ import styles from '../styles/home.module.css'; // Importe o módulo CSS
 
 export default function Home() {
   return (
-    <div>
-      <h1>Bem-vindo à Página Inicial!</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Bem-vindo à Página Inicial!</h1>
       <p>Conteúdo da homepage no Vercel.</p>
 	  <Link href="/login">
         <button>Ir para a Página de Login</button>
