8e9c79b74c6baf2914227d1d39408affb0ef28e0
VitorFormiga <jvformiga@gmail.com>
2025-06-11 10:10:03 -0300

    11.06.2025  09

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+// pages/index.js
+import React from 'react';
+import Link from 'next/link'; // Importe o componente Link
+
 export default function Home() {
-    return (
-	<div>
-        <div>VitorFormiga</div>
-		<div>alalaal222</div>
-		
-		</div>
-    )
+  return (
+    <div>
+      <h1>Bem-vindo à Página Inicial!</h1>
+      <p>Conteúdo da homepage no Vercel.</p>
+	  <Link href="/login">
+        <button>Ir para a Página de Login</button>
+      </Link>
+    </div>
+  );
 }
